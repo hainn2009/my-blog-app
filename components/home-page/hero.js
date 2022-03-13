c31fb20dfe1f59c490ef7f99e5e3d6c823f6eae0
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-13 08:34:56 +0700

    add hero page

---
+++ components/home-page/hero.js
@@ -0,0 +1,12 @@
+import classes from './hero.module.css'
+import Image from 'next/image'
+
+export default function Hero(){
+    return <section className={classes.hero}>
+        <div className={classes.image}>
+            <Image src="/images/site/hai.jpg" alt="An image showing Hai" width={300} height={300} />
+        </div>
+        <h1>Hi, I'm Hai</h1>
+        <p>I blog about web development - especially frontend framework like React.</p>
+    </section>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+import { Fragment } from 'react'
+import Hero from '../components/home-page/hero'
+
 export default function HomePage() {
-  
+  return <Fragment>
+      <Hero />
+  </Fragment>
 }
