87bb07048e28f8df09586d9fd640055d94d32c39
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-15 21:11:17 +0700

    Add post item component

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import clesses from "./post-item.module.css";
 
-export default function PostItem({ post }) {
-  return <li key={post.id}></li>;
+export default function PostItem({ post: { id, title, image, exerpt, date, slug } }) {
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const imagePath = `/images/posts/${slug}/${image}`;
+  return (
+    <li key={post.id} className={classes.post}>
+      <Link>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{exerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 }
