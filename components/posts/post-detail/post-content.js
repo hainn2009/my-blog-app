4b4b26a684cc46600679f3fafdfcb13abaf81b49
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-18 20:41:54 +0700

    Add post detail page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,21 @@
+import classes from "./post-content.module.css";
+import PostHeader from "./post-header";
+
+const post = {
+  title: "Getting started with Next.js",
+  image: "getting-started-nextjs.png",
+  date: "2022-03-16",
+  slug: "getting-started-with-nextjs",
+  content: "# This is the first post",
+};
+
+export default function PostContent() {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  return (
+    <article>
+      <PostHeader title={post.title} image={imagePath} />
+      {post.content}
+    </article>
+  );
+}
