1ba245e319740587a727523142320074e4758a60
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-19 09:11:35 +0700

    Add getStaticprops for index page

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,21 @@ import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const posts = [
-  {
-    title: "Getting started with Next.js",
-    image: "getting-started-nextjs.png",
-    exerpt: "Something about Nextjs with server-side rendering",
-    date: "2022-03-16",
-    slug: "getting-started-with-nextjs",
-  },
-  {
-    title: "Getting started with Next.js1",
-    image: "getting-started-nextjs1.png",
-    exerpt: "Something about Nextjs with server-side rendering",
-    date: "2022-03-16",
-    slug: "getting-started-with-nextjs1",
-  },
-  {
-    title: "Getting started with Next.js2",
-    image: "getting-started-nextjs2.png",
-    exerpt: "Something about Nextjs with server-side rendering",
-    date: "2022-03-16",
-    slug: "getting-started-with-nextjs2",
-  },
-];
-
-export default function HomePage() {
+export default function HomePage({ posts }) {
   return (
     <Fragment>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      posts: getFeaturedPosts(),
+    },
+  };
 }
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,8 +1,8 @@
 ---
 title: "Getting Started With NextJS"
 date: "2022-03-18"
-image: "getting-started-with-nextjs.png"
-excerpt: "Something about Nextjs with server-side rendering"
+image: "getting-started-nextjs.png"
+exerpt: "Something about Nextjs with server-side rendering"
 isFeatured: true
 ---
 
