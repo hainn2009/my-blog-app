f5068747029eec2d48d7eaff4c7d5acde8b4722e
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-19 10:11:28 +0700

    Add GetStaticProps for a detail post

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,15 +3,7 @@ import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
-const post = {
-  title: "Getting started with Next.js",
-  image: "getting-started-nextjs.png",
-  date: "2022-03-16",
-  slug: "getting-started-with-nextjs",
-  content: "# This is the first post",
-};
-
-export default function PostContent() {
+export default function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,26 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-export default function PostDetailPage() {
-  return <PostContent />;
+export default function PostDetailPage({ post }) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  return {
+    props: {
+      post: getPostData(context.params.slug),
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: getPostsFiles().map((postFile) => ({
+      params: {
+        slug: postFile.replace(/\.md$/, ""),
+      },
+    })),
+    fallback: false,
+  };
 }
