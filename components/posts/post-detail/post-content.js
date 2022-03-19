141498630ea06c4a9b05af03f506fcf21c0c68b3
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-19 11:45:38 +0700

    Override react markdown render

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,36 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
 export default function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
-
+  const customRenders = {
+    img({ node, ...image }) {
+      return (
+        <div className={classes.image}>
+          <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+        </div>
+      );
+    },
+    // not working
+    // p({ node, ...props }) {
+    //   console.log(node);
+    //   if (node.children[0].type === "img")
+    //     return (
+    //       <div className={classes.image}>
+    //         <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+    //       </div>
+    //     );
+    //   return <p>{node.children}</p>;
+    // },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      {/* <ReactMarkdown renderers={customRenders}>{post.content}</ReactMarkdown> */}
+      <ReactMarkdown components={customRenders}>{post.content}</ReactMarkdown>
     </article>
   );
 }
