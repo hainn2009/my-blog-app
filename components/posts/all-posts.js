f50a3a0951ebd1f4e23d197880cb039eb8866c89
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-24 22:14:35 +0700

    Small things

--- components/posts/all-posts.js
+++ components/posts/all-posts.js
@@ -4,7 +4,7 @@ import PostGrid from "./post-grid";
 export default function AllPosts(props) {
   return (
     <section className={classes.posts}>
-      <h1>All Posts</h1>
+      <h1>All Posts!!!</h1>
       <PostGrid posts={props.posts} />
     </section>
   );
