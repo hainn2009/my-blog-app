d2167779d0754dafb4b830518c69502d6fa1d615
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-29 09:29:26 +0700

    Update the image

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,11 +1,13 @@
-import classes from "./hero.module.css";
 import Image from "next/image";
+
+import classes from "./hero.module.css";
+import profilePic from "../../public/images/site/hai.jpg";
 
 export default function Hero() {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
-        <Image src="/images/site/hai.jpg" alt="An image showing Hai" width={300} height={300} />
+        <Image src={profilePic} alt="An image showing Hai" width={300} height={300} />
       </div>
       <h1>Hi, I&apos;m Hai</h1>
       <p>I blog about web development - especially frontend framework like React.</p>
