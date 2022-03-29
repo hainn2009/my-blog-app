a2013fd53834a41541991b7c4182c2769c87fb07
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-29 08:35:59 +0700

    update next.config file about image

--- next.config.js
+++ next.config.js
@@ -2,26 +2,32 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 const nextConfig = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER)
-    return {
-      env: {
-        mongodb_url: "",
-        mongodb_username: "",
-        mongodb_password: "",
-        mongodb_clustername: "",
-        mongodb_database: "123",
-      },
-    };
+  // if (phase === PHASE_DEVELOPMENT_SERVER)
+  //   return {
+  //     env: {
+  //       mongodb_url: "",
+  //       mongodb_username: "",
+  //       mongodb_password: "",
+  //       mongodb_clustername: "",
+  //       mongodb_database: "123",
+  //     },
+  //   };
 
+  // return {
+  //   env: {
+  //     mongodb_url: "",
+  //     mongodb_username: "",
+  //     mongodb_password: "",
+  //     mongodb_clustername: "",
+  //     mongodb_database: "456",
+  //   },
+  // };
   return {
-    env: {
-      mongodb_url: "",
-      mongodb_username: "",
-      mongodb_password: "",
-      mongodb_clustername: "",
-      mongodb_database: "456",
+    images: {
+      domains: ["localhost"],
     },
   };
 };
 
 module.exports = nextConfig;
+
