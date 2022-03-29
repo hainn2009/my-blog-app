656e4c37fa897a0c1a72cbe512d4021f58f03d26
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-29 08:38:47 +0700

    all posts header update

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -8,7 +8,7 @@ export default function AllPostsPage({ posts }) {
   return (
     <Fragment>
       <Head>
-        <title>All Posts</title>
+        <title>All Posts123</title>
         <meta name="description" content="A list of all programmig-related tutorials and posts" />
       </Head>
       <AllPosts posts={posts} />
