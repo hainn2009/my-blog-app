7f1adae96b1b00060d61e7e7222efc8fcbff84c8
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-03-29 08:40:33 +0700

    Comments all next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,33 @@
-/** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+// /** @type {import('next').NextConfig} */
+// const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const nextConfig = (phase) => {
-  // if (phase === PHASE_DEVELOPMENT_SERVER)
-  //   return {
-  //     env: {
-  //       mongodb_url: "",
-  //       mongodb_username: "",
-  //       mongodb_password: "",
-  //       mongodb_clustername: "",
-  //       mongodb_database: "123",
-  //     },
-  //   };
+// const nextConfig = (phase) => {
+//   // if (phase === PHASE_DEVELOPMENT_SERVER)
+//   //   return {
+//   //     env: {
+//   //       mongodb_url: "",
+//   //       mongodb_username: "",
+//   //       mongodb_password: "",
+//   //       mongodb_clustername: "",
+//   //       mongodb_database: "123",
+//   //     },
+//   //   };
 
-  // return {
-  //   env: {
-  //     mongodb_url: "",
-  //     mongodb_username: "",
-  //     mongodb_password: "",
-  //     mongodb_clustername: "",
-  //     mongodb_database: "456",
-  //   },
-  // };
-  return {
-    images: {
-      domains: ["localhost"],
-    },
-  };
-};
+//   // return {
+//   //   env: {
+//   //     mongodb_url: "",
+//   //     mongodb_username: "",
+//   //     mongodb_password: "",
+//   //     mongodb_clustername: "",
+//   //     mongodb_database: "456",
+//   //   },
+//   // };
+//   return {
+//     images: {
+//       domains: ["localhost"],
+//     },
+//   };
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
 
