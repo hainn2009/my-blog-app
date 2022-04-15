93fbba88df8db53bceac26fcb8b2eb401685ca3b
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-04-15 10:25:13 +0700

    update

--- pages/test/customForm.js
+++ pages/test/customForm.js
@@ -1,10 +1,9 @@
 import InputComponent from "./inputComponent";
 
 export default function CustomsForm(props) {
-    
   return (
     <div>
-      <InputComponent type="text" value={} onChange={}/>
+      <InputComponent type="text" value={1} onChange={1} />
     </div>
   );
 }
