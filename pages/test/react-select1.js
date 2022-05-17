a5f44cc9d1309b2229388406e95936ca3a960531
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-05-17 08:38:59 +0700

    update

--- pages/test/react-select1.js
+++ pages/test/react-select1.js
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import CreatableSelect from "react-select/creatable";
 // import { ColourOption, colourOptions } from "../data";
 import { ActionMeta, OnChangeValue } from "react-select";
+import Select from "react-select";
+
+const options1 = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
 
 const options = [
   { label: "Green", value: "green" },
@@ -34,7 +41,16 @@ export default class CreatableSingle extends Component {
   render() {
     return (
       <div style={{ width: "400px" }}>
-        <CreatableSelect isClearable isMulti onChange={this.handleChange} onInputChange={this.handleInputChange} value={this.state.colors} options={options} />;
+        <CreatableSelect
+          isClearable={false}
+          isMulti
+          onChange={this.handleChange}
+          onInputChange={this.handleInputChange}
+          value={this.state.colors}
+          options={options}
+          isDisabled
+        />
+        <Select isMulti options={options1} />
       </div>
     );
   }
