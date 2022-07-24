e2c663ed4bef993650b50644fb6e5d9fc5dd7f55
Nguyen Ngoc Hai <hainn2009@gmail.com>
2022-07-24 15:05:29 +0700

    update

--- pages/test/react-select1.js
+++ pages/test/react-select1.js
@@ -6,52 +6,52 @@ import { ActionMeta, OnChangeValue } from "react-select";
 import Select from "react-select";
 
 const options1 = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
 ];
 
 const options = [
-  { label: "Green", value: "green" },
-  { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Red", value: "red" },
 ];
 
 export default class CreatableSingle extends Component {
-  //   handleChange = (newValue, actionMeta) => {
-  //     console.group("Value Changed");
-  //     console.log(newValue);
-  //     console.log(`action: ${actionMeta.action}`);
-  //     console.groupEnd();
-  //   };
-  state = {
-    colors: [{ label: "test", value: "test" }],
-  };
-  handleChange = (colors) => {
-    console.log(colors);
-    this.setState({
-      colors,
-    });
-  };
-  handleInputChange = (inputValue, actionMeta) => {
-    // console.group("Input Changed");
-    // console.log(inputValue);
-    // console.log(`action: ${actionMeta.action}`);
-    // console.groupEnd();
-  };
-  render() {
-    return (
-      <div style={{ width: "400px" }}>
-        <CreatableSelect
-          isClearable={false}
-          isMulti
-          onChange={this.handleChange}
-          onInputChange={this.handleInputChange}
-          value={this.state.colors}
-          options={options}
-          // isDisabled
-        />
-        <Select isMulti options={options1} />
-      </div>
-    );
-  }
+    //   handleChange = (newValue, actionMeta) => {
+    //     console.group("Value Changed");
+    //     console.log(newValue);
+    //     console.log(`action: ${actionMeta.action}`);
+    //     console.groupEnd();
+    //   };
+    state = {
+        colors: [{ label: "test", value: "test" }],
+    };
+    handleChange = (colors) => {
+        console.log(colors);
+        this.setState({
+            colors,
+        });
+    };
+    handleInputChange = (inputValue, actionMeta) => {
+        // console.group("Input Changed");
+        // console.log(inputValue);
+        // console.log(`action: ${actionMeta.action}`);
+        // console.groupEnd();
+    };
+    render() {
+        return (
+            <div style={{ width: "400px" }}>
+                <CreatableSelect
+                    isClearable={false}
+                    isMulti
+                    onChange={this.handleChange}
+                    onInputChange={this.handleInputChange}
+                    value={this.state.colors}
+                    options={options}
+                    isDisabled
+                />
+                <Select isMulti options={options1} />
+            </div>
+        );
+    }
 }
